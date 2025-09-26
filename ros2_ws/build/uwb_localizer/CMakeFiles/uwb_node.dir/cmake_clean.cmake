421e09f2a00bbb9e03be2408da37746dc222bf0e
Joao Passos <jpassos@andrew.cmu.edu>
2025-09-25 20:04:44 -0400

    first colcon build

---
+++ ros2_ws/build/uwb_localizer/CMakeFiles/uwb_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uwb_node.dir/src/uwb_node.cpp.o"
+  "CMakeFiles/uwb_node.dir/src/uwb_node.cpp.o.d"
+  "uwb_node"
+  "uwb_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uwb_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
